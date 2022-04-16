efbaaa2d0cb0cab51ada941e429ba67abda4731a
Sanush Gowda <brsanushgowda@gmail.com>
2022-04-16 18:39:48 +0530

    fix env variable bug

--- startup/db.js
+++ startup/db.js
@@ -2,9 +2,8 @@ const mongoose = require('mongoose');
 const config = require('config');
 
 module.exports = function(){
-    const db = config.get('helpMyInterviews_db');
+    const db = config.get('db');
     mongoose.connect(db)
     .then(() => { console.log('Connected to Mongo-DB....') })
     .catch(err => console.error('Unable to connect to Mongo-DB....'));
-
 }
