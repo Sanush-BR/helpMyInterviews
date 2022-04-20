2b9d4b221d7f7052316eff2428b8298552accbc1
Sanush Gowda <brsanushgowda@gmail.com>
2022-04-20 20:35:34 +0530

    Change the env file to hard coded constr value

--- startup/db.js
+++ startup/db.js
@@ -3,7 +3,8 @@ const config = require('config');
 
 module.exports = function(){
 
-    const db = config.get('db');
+    // const db = config.get('db');
+    const db = 'mongodb+srv://SanushBR:sanush@123@cluster0.j4vrt.mongodb.net/Company_db?retryWrites=true&w=majority';
     mongoose.connect(db)
     .then(() => { console.log(`Connected to ${db}....`)})
     .catch(err => console.error('Unable to connect to Mongo-DB....'));
